441f66ae693134e5ab3dfec57f751640cb033d19
MAC2 <igate@din69000176.corp.capgemini.com>
2018-06-19 16:26:39 +0530

    Added comment.

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Added comment")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
